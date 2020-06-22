80cc13a1e70cdb76507c05931ce2c9aefc3ee62d
Andrew Cabey <me@acabey.xyz>
2020-06-21 20:19:05 -0400

    Refactored MediumService to constructor singleton

--- src/services/MediumService.js
+++ src/services/MediumService.js
@@ -1,61 +1,62 @@
-//const url = 'https://acabey-server-java.herokuapp.com/api';
 import dummyMedia from "../utils/DummyMediaData";
 
-const url = 'http://localhost:8080/api';
+class MediumService {
 
-export const createMedium = (userId, medium) =>
-    fetch(`${url}/users/${userId}/media`, {
-        method: 'POST',
-        body: JSON.stringify(medium),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json());
+    url = 'http://localhost:8080/api';
+    //const url = 'https://acabey-server-java.herokuapp.com/api';
 
-export const findMediaForUser = (userId) =>
-    fetch(`${url}/users/${userId}/media`)
-        .then(response => response.json());
+    constructor() {
+        //this.tvdbClient = new TVDBClient();
+        const TVDB = require('node-tvdb');
+        this.tvdb = new TVDB('e5094420c444a38c3b46f926de91dde3');
+    }
 
-export const findMediumById = (mediumId) =>
-    fetch(`${url}/media/${mediumId}`, {
-        method: 'GET'
-    })
-        .then(response => response.json());
+    createMedium = (userId, medium) =>
+        fetch(`${this.url}/users/${userId}/media`, {
+            method: 'POST',
+            body: JSON.stringify(medium),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(response => response.json());
 
-export const findAllMedia = (mediumId) =>
-    fetch(`${url}/media`, {
-        method: 'GET'
-    })
-        .then(response => response.json());
+    findMediaForUser = (userId) =>
+        fetch(`${this.url}/users/${userId}/media`)
+            .then(response => response.json());
 
-export const updateMedium = (mediumId, medium) =>
-    fetch(`${url}/media/${mediumId}`, {
-        method: 'PUT',
-        body: JSON.stringify(medium),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json());
+    findMediumById = (mediumId) =>
+        fetch(`${this.url}/media/${mediumId}`, {
+            method: 'GET'
+        })
+            .then(response => response.json());
 
-export const deleteMedium = (mediumId) =>
-    fetch(`${url}/media/${mediumId}`, {
-        method: 'DELETE'
-    })
-        .then(response => response.json());
+    findAllMedia = (mediumId) =>
+        fetch(`${this.url}/media`, {
+            method: 'GET'
+        })
+            .then(response => response.json());
 
-export const searchMediaByTitle = (title) => {
-    return JSON.stringify(dummyMedia)
-};
+    updateMedium = (mediumId, medium) =>
+        fetch(`${this.url}/media/${mediumId}`, {
+            method: 'PUT',
+            body: JSON.stringify(medium),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(response => response.json());
 
+    deleteMedium = (mediumId) =>
+        fetch(`${this.url}/media/${mediumId}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json());
 
-export default {
-    createMedium,
-    findMediaForUser,
-    findMediumById,
-    findAllMedia,
-    updateMedium,
-    deleteMedium,
-    searchMediaByTitle
+    searchMediaByTitle = (title) => {
+        return JSON.stringify(dummyMedia)
+    };
 }
+
+
+export default new MediumService();
